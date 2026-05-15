4edfd07e31ad0503214a49719009d37a3fdd4639
Sivn <sivn@foxmail.com>
2026-05-15 11:29:28 +0800

    manage struct
    manage struct

---
+++ src/client/client.java
@@ -0,0 +1,7 @@
+package client;
+
+public class client {
+    public static void main(String[] args) {
+        System.out.println("HSR Client started.");
+    }
+}
---
+++ src/server/server.java
@@ -0,0 +1,7 @@
+package server;
+
+public class server {
+    public static void main(String[] args) {
+        System.out.println("HSR Server started.");
+    }
+}
